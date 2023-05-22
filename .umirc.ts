cb07a5d4f2df3b62dcb6187e3fffadf537186838
likangle <me@lkangle.cn>
2023-05-23 00:47:07 +0800

    fix: 非hash模式

--- .umirc.ts
+++ .umirc.ts
@@ -3,8 +3,6 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'MagicPaper',
   mode: 'site',
-  logo: "/helpcenter/newlogo.png",
-  base: '/helpcenter',
-  publicPath: '/helpcenter/'
+  logo: "/newlogo.png",
   // more config: https://d.umijs.org/config
 });
